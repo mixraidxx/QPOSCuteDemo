64763dd909940d29772c4e6bb5bb58f286489feb
David Enriquez <denriquezs1200@alumno.ipn.mx>
2021-12-23 23:08:41 -0600

    Initial Commit

---
+++ QPOSCuteDemo/QPOSCuteDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QPOSCuteDemoApp.swift
+//  QPOSCuteDemo
+//
+//  Created by David Enriquez solis on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct QPOSCuteDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
